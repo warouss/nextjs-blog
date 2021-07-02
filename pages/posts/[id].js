e50c67ed0294ca283a9e224030e55b92aab0daea
Walid Arouss <warouss@aliznet.fr>
2021-07-02 16:23:32 +0200

    Dynamic Routes : getStaticPaths, getStaticProps

---
+++ pages/posts/[id].js
@@ -0,0 +1,34 @@
+import Layout from '../../components/layout'
+import { getAllPostIds, getPostData } from '../../lib/posts'
+
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      {postData.title}
+      <br />
+      {postData.id}
+      <br />
+      {postData.date}
+    </Layout>
+  )
+}
+
+/**
+ * In this function, we need to return a list of possible values for id
+ */
+export async function getStaticPaths() {
+  const paths = getAllPostIds()
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+export async function getStaticProps({ params }) {
+  const postData = getPostData(params.id)
+  return {
+    props: {
+      postData
+    }
+  }
+}
